4e121119954832a7e7159e9478ab167173749952
kyri21 <kyriarthur@gmail.com>
2025-04-15 13:56:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import json
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
-st.set_page_config(page_title="Yorgios Test", page_icon="🚀")
+st.title("🚀 Yorgios App")
 
-st.title("🚀 Yorgios App déployée sur Railway")
-st.success("Ça fonctionne ! 🎉")
+# Récupérer les secrets depuis Render
+mail_password = os.getenv("MAIL_PASSWORD")
+json_str = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+service_account_info = json.loads(json_str)
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
+client = gspread.authorize(creds)
+
+st.success("Connexion réussie à Google Sheets ✅")
